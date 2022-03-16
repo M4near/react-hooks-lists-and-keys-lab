31c0a3c3f55c43ef8421d9abe04e9dbd9cd53d01
Michael Fournier <michaeljfournier5@gmail.com>
2022-03-16 18:31:03 -0400

    commit tree 0e49e7059c4bac40a3e872f50e124c948aa93dce

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const links = ["#home", "#about", "#projects"];
+  console.log(links);
+  const navLinks = links.map((link) => {
+    return <a key={link} href={link}>{link}</a>
+  })
+  return <nav>{navLinks}</nav>;
 }
 
 export default NavBar;
